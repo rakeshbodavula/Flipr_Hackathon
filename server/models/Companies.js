5256cb3ce9945f44101f32fc9e5311cd12765e9d
RAKESH BODAVULA <rakeshbodavula2002@gmail.com>
2023-01-14 12:44:16 +0530

    server

---
+++ server/models/Companies.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+const companySchema = new mongoose.Schema({
+    CompanyName: {
+        type: String,
+        required: true,
+        unique:true
+    },
+    Date: {
+        type: String,
+        required: true
+    },
+    Open: {
+        type: Number,
+        required: true
+    },
+    High: {
+        type: Number,
+        required: true
+    },
+    Low: {
+        type: Number,
+        required: true
+    },
+    Close: {
+        type: Number,
+        required: true
+    },
+    AdjClose: {
+        type: Number,
+        required: true
+    },
+    Volume: {
+        type: Number,
+        required: true
+    }
+
+}, { collection: 'companies' })
+
+
+const Company = mongoose.model('Company', companySchema)
+
+module.exports = Company
