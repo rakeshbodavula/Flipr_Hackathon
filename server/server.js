7f71eccd946eed40a9fa3b8ac551c512f3c189d5
RAKESH BODAVULA <rakeshbodavula2002@gmail.com>
2023-01-14 14:58:04 +0530

    conncted backend

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
-const {checkUser} = require('./middlewares/authMiddleware')
-const authRoutes = require('./routes/authRoutes')
 const cors = require('cors')
+
+const Company = require('./models/Company')
+const Stock = require('./models/Stock')
+const User = require('./models/User')
 
 const app = express()
 
@@ -13,26 +14,22 @@ const app = express()
 app.use(express.urlencoded({extended : true}))
 app.use(express.json())
 app.use(bodyParser.json())
-app.use(cookieParser())
 app.use(cors())
 
 
 // database connectivitycls
-const dbURI = 'mongodb://localhost:27017/ffsd1'
+// const dbURI = 'mongodb://localhost:27017/flipr'
+// const dbURI = 'mongodb+srv://flipr:flipr%40hackathon@cluster0.dkyw7jy.mongodb.net/test'
+const dbURI = 'mongodb+srv://flipr:flipr%40hackathon@cluster0.dkyw7jy.mongodb.net/?retryWrites=true&w=majority'
 
 const port = process.env.PORT || 9999
-mongoose.connect(dbURI)
+mongoose.connect(dbURI,{useNewUrlParser: true})
         .then(app.listen(port,(err)=>{
                 console.log(`http://localhost:${port}/`)
         }))
         .catch((err)=> console.log(err))
 
 
-
-// routes
-
-// app.get('*',checkUser)
-// app.post('*',checkUser)
-app.use(authRoutes)
-
-
+app.get('/',(req,res)=>{
+    res.send("hii")
+})
