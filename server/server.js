a4f3d6501d06f0760ef993721b472ea9e16c114e
RAKESH BODAVULA <rakeshbodavula2002@gmail.com>
2023-01-14 13:48:56 +0530

    del

--- server/server.js
+++ server/server.js
@@ -1,2 +1,38 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const {checkUser} = require('./middlewares/authMiddleware')
+const authRoutes = require('./routes/authRoutes')
+const cors = require('cors')
+
+const app = express()
+
+
+// middleware
+app.use(express.urlencoded({extended : true}))
+app.use(express.json())
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(cors())
+
+
+// database connectivitycls
+const dbURI = 'mongodb://localhost:27017/ffsd1'
+
+const port = process.env.PORT || 9999
+mongoose.connect(dbURI)
+        .then(app.listen(port,(err)=>{
+                console.log(`http://localhost:${port}/`)
+        }))
+        .catch((err)=> console.log(err))
+
+
+
+// routes
+
+// app.get('*',checkUser)
+// app.post('*',checkUser)
+app.use(authRoutes)
+
+
