62f3e0c58be16eaa848accf44aa3523f3eca58f3
subhash <ksubhash8055@gmail.com>
2023-01-16 13:02:23 +0530

    login ui changed

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -31,15 +31,15 @@ const Login = () => {
     }
     return (
         <>
-            <section className="vh-100" style={{ backgroundColor: '#9A616D' }}>
+            <section className="vh-100" style={{ backgroundImage: "" }}>
                 <div className="container py-5 h-100">
                     <div className="row d-flex justify-content-center align-items-center h-100">
                         <div className="col col-xl-10">
                             <div className="card" style={{ borderRadius: '1rem' }}>
                                 <div className="row g-0">
                                     <div className="col-md-6 col-lg-5 d-none d-md-block">
-                                        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp"
-                                            alt="login form" className="img-fluid" style={{ borderRadius: '1rem 0 0 1rem' }} />
+                                        <img src="https://www.pvot.in/content/images/size/w300/blogs/markets_Ul3m381.jpg"
+                                            alt="login form" className="img-fluid" style={{ borderRadius: '1rem 0 0 1rem', backgroundSize: "cover", height: "600px", width: "400px" }} />
                                     </div>
                                     <div className="col-md-6 col-lg-7 d-flex align-items-center">
                                         <div className="card-body p-4 p-lg-5 text-black">
